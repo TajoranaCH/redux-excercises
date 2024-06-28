ee52ab2a79c7f02ae071c40ea238b1964a8a9870
Japneet Singh <japneetsingh1807@outlook.it>
2024-06-28 12:45:21 +0200

    organize + anecdotes

---
+++ anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+    const dispatch = useDispatch()
+    const anecdotes = useSelector(state => state)
+
+    return (<>
+        {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>vote</button>
+          </div>
+        </div>
+      )}</>)
+}
+
+export default AnecdoteList
